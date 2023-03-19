02012e408a5087b5b0f6b6c6962f61b59d1f9ff3
Cristian Puncea <cristian.puncea@gmail.com>
2023-03-19 12:43:33 +0200

    Added a first schema and model to the app

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
+const Workout = require("../models/workoutModel");
 
 // GET all workouts
 router.get("/", (req, res) => {
@@ -13,8 +14,14 @@ router.get("/:id", (req, res) => {
 });
 
 // POST a new workout
-router.post("/", (req, res) => {
-  res.json({ mssg: "POST a new workout" });
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // DELETE a workout
