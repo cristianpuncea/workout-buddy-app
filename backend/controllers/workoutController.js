7683d8c0afafb21a04ac971552be5885e71428b7
Cristian Puncea <cristian.puncea@gmail.com>
2023-03-29 21:50:32 +0300

    Adding more user friendly error handling

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -29,6 +29,26 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
   const { title, reps, load } = req.body;
 
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+
+  if (!load) {
+    emptyFields.push("load");
+  }
+
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all the fields", emptyFields });
+  }
+
   // add doc to db
   try {
     const workout = await Workout.create({ title, reps, load });
@@ -81,5 +101,5 @@ module.exports = {
   getWorkout,
   createWorkout,
   deleteWorkout,
-  updateWorkout
+  updateWorkout,
 };
